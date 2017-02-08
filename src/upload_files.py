7a202d7e0a566d7b5bba915054c853a03f41f360
adamkrieger <adamkrieger.ca@gmail.com>
2017-02-08 13:49:13 -0600

    Improved content type detection so that it works with multiple filetypes.

--- src/upload_files.py
+++ src/upload_files.py
@@ -1,22 +1,31 @@
 """Upload files"""
 import argparse
+import os
 
 from common import basic
 
 PARSER = argparse.ArgumentParser()
 PARSER.add_argument('--bucket', help='Name of the destination bucket.', required=True)
 
-def __upload_file__(bucket, file_src, file_dest):
+def __upload_file__(bucket, file_src, file_dest, content_types):
     extra_args = {}
 
-    if file_src.endswith('.html'):
-        extra_args['ContentType'] = 'text/html'
+    _, file_extension = os.path.splitext(file_src)
+
+    if file_extension in content_types:
+        extra_args['ContentType'] = content_types[file_extension]
 
     bucket.upload_file(file_src, file_dest, ExtraArgs=extra_args)
+
+CONTENT_TYPES = {
+    ".html": "text/html",
+    ".xml": "text/xml",
+    ".css": "text/css"
+}
 
 ARGUMENTS = PARSER.parse_args()
 STHREE = basic.get_s3_resource()
 BUCKET = STHREE.Bucket(ARGUMENTS.bucket)
 
-__upload_file__(BUCKET, './tmp/index.html', 'index.html')
-__upload_file__(BUCKET, './tmp/404.html', '404.html')
+__upload_file__(BUCKET, './tmp/index.html', 'index.html', CONTENT_TYPES)
+__upload_file__(BUCKET, './tmp/404.html', '404.html', CONTENT_TYPES)
