66437b250126f3ee6f87b1a7e4389b7147755859
adamkrieger <adamkrieger.ca@gmail.com>
2017-02-09 14:38:31 -0600

    Fixed error in filesys which wasn't trimming paths correctly.

--- src/common/basic.py
+++ src/common/basic.py
@@ -6,7 +6,7 @@ def get_s3():
     """Returns an s3 client"""
     client = boto3.client(
         's3',
-        aws_access_key_id=os.environ.get('AWS_ACCESS_KEY'),
+        aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),
         aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY')
     )
 
--- src/common/filesys.py
+++ src/common/filesys.py
@@ -1,15 +1,16 @@
 """File System Interaction"""
 import os
 
-def __full_rel_pair__(true_root, rel_root, name):
-    full_path = os.path.join(rel_root, name)
-    return full_path, full_path.lstrip(true_root)
+def __full_rel_pair__(root, this_file_root, this_filename):
+    full_path = os.path.join(this_file_root, this_filename)
+    relative_path = full_path[len(root):]
+    return full_path, relative_path
 
 def get_files_recursively(directory):
     """Returns filepaths recursively."""
     filepaths = [
-        __full_rel_pair__(directory, root, name)
-        for root, _, files in os.walk(directory)
-        for name in files]
+        __full_rel_pair__(directory, each_directory, each_filename)
+        for each_directory, _, files in os.walk(directory)
+        for each_filename in files]
 
     return filepaths
