30ad980067852780c1b3bd9d4fb9593d891108bb
adamkrieger <adamkrieger.ca@gmail.com>
2017-02-09 15:14:19 -0600

    Also string leading slash from filepath.

--- src/common/filesys.py
+++ src/common/filesys.py
@@ -3,7 +3,8 @@ import os
 
 def __full_rel_pair__(root, this_file_root, this_filename):
     full_path = os.path.join(this_file_root, this_filename)
-    relative_path = full_path[len(root):]
+    root_with_trailing = os.path.join(root, '')
+    relative_path = full_path[len(root_with_trailing):]
     return full_path, relative_path
 
 def get_files_recursively(directory):
